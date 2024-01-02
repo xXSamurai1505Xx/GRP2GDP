987a47f5dcd9e01f616919f2f55154cbf5a192a1
xXSamurai1505Xx <davidtoh1505@gmail.com>
2024-01-02 15:26:49 +0800

    update

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -10,8 +10,7 @@ public class PlayerHealth : MonoBehaviour
     [SerializeField] private Animator animator; // Reference to the Animator component
 
     private bool isDead = false; // Flag to check if the player is dead
-    public GameObject gameOverScreen;
-    public GameObject gameUIScreen;
+
     private void Start()
     {
         currentHealth = maxHealth;
@@ -80,8 +79,5 @@ public class PlayerHealth : MonoBehaviour
         Time.timeScale = 0f;  // This freezes the game
 
         Debug.Log("Game has been frozen after player death!");
-        gameUIScreen.SetActive(false);
-        gameOverScreen.SetActive(true);
-
     }
 }
